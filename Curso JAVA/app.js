169ce89c493d0d6b6c7dc32abfe6ed0d0362ddf5
ANGEL IVAN PALOMARES MORALES <124049019@upq.edu.mx>
2025-09-09 07:54:19 -0600

    13.-Se prueba el uso de dos if y un else en donde el las comparaciones darán dos resultados ya que se hacen dos comparaciones con resultado positivo

--- Curso JAVA/app.js
+++ Curso JAVA/app.js
@@ -4,6 +4,10 @@ if (score > 30)
 {
     console.log("You need to practice more ")
 }
+if (score >15)
+{
+    console.log("Estas mejorando")
+}
 else
 {
   console.log("You need to follow this tutorial")   
