7b0393fdd1aaa40eb76d2972672db9cb2f5f1a3e
ANGEL IVAN PALOMARES MORALES <124049019@upq.edu.mx>
2025-09-09 07:27:12 -0600

    Se prueba el uso de las variables y de los operadores

--- Curso JAVA/app.js
+++ Curso JAVA/app.js
@@ -1,4 +1,5 @@
-console.log("esta es una linea se codigo");
-console.log(14.5);
-console.log([1,2,3,4]);
-console.log({"username": "ryan", "score":70.4});
+let name="John";
+let lastname="Carter";
+let completeName= name + lastname;
+
+console.log(completeName);
