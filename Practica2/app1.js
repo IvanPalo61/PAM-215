0c054d6f6daeebc43de132cfafd1b6bb98aa3058
ANGEL IVAN PALOMARES MORALES <124049019@upq.edu.mx>
2025-09-22 20:10:44 -0600

    Destructuracion
    Se extrajo los valores de nombre, edad y ciudad usando destructuración.
    Luego, muestra un mensaje como: "Me llamo Ivan Isay, tengo 37 años y vivo en Qro."

---
+++ Practica2/app1.js
@@ -0,0 +1,15 @@
+const persona = {
+nombre:"Ivan Isay",
+edad:37, 
+direccion:
+{
+    ciudad:"Qro",
+   pais:"Mexico"
+
+}
+}
+;
+const { nombre, edad , direccion}=persona;
+console.log ('Me llamo '+ nombre + " tengo " + edad + " años y vivo en "+ direccion.ciudad);
+
+
