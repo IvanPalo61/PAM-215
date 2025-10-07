e6fe2e80fcc0d8e769b51f5ab500b3067286b6b7
ANGEL IVAN PALOMARES MORALES <124049019@upq.edu.mx>
2025-10-06 19:29:12 -0600

    Update app4.js

--- Practica3/app4.js
+++ Practica3/app4.js
@@ -14,3 +14,15 @@ async function obtenerDatos() {
     console.log(resultado);
 }
 obtenerDatos();
+// Ejemplo de destructuración solicitado
+const persona = {
+    nombre: "Ivan Isay",
+    edad: 37,
+    direccion: {
+        ciudad: "Qro",
+        pais: "MX"
+    }
+};
+
+const { nombre, edad, direccion: { ciudad } } = persona;
+console.log(`Me llamo ${nombre}, tengo ${edad} años y vivo en ${ciudad}.`);
