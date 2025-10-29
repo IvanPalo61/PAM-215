6e1b1ab4deab5571bc6431f0b5bd88b5fe824fa8
ANGEL IVAN PALOMARES MORALES <124049019@upq.edu.mx>
2025-10-28 20:28:15 -0600

    ActivityIndicator completado

--- intro/screens/ActivityIndicatorScreen.js
+++ intro/screens/ActivityIndicatorScreen.js
@@ -1,9 +1,51 @@
-import {Text, StyleSheet, View} from 'react-native';
+import { Text, StyleSheet, View, Button, ActivityIndicator } from 'react-native';
+import React, { useState } from 'react';
 
-export default function ActivityIndicatorScreen () {
+export default function ActivityIndicatorScreen() {
+  const [loading, setLoading] = useState(false);
+
+  const startLoading = () => {
+    setLoading(true);
+    setTimeout(() => setLoading(false), 3000);
+  };
+
+  if (loading) {
+    return (
+      <View style={styles.container}>
+        <View style={styles.loaderContainer}>
+          <ActivityIndicator 
+            size="large" 
+            color="#1964d3ff" 
+            animating={true}
+            hidesWhenStopped={true}
+          />
+          <Text>Cargando.........</Text>
+        </View>
+      </View>
+    );
+  }
+
   return (
-    <View>
-      <Text>Proximamente</Text>
-    </View>
+    <View style={styles.container}>
+      <Text style={styles.title}>ActivityIndicator</Text>
+      <Button title="Iniciar Carga" onPress={startLoading} />
+    </View> 
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#96c7d3ff',
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginBottom: 20,
+  },
+  loaderContainer: {
+    alignItems: 'center',
+  },
+});
